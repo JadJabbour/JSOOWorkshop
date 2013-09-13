a5250c7cd5780966310d60644e74df66f7459fb4
Jad Jabbour <jabbourj.87@gmail.com>
2013-09-13 13:47:03 +0300

    tcm6 changes #1

--- tcm6.js
+++ tcm6.js
@@ -9,51 +9,75 @@ Object.prototype.base = function(){
 	return this.__proto__.__proto__;
 };
 
+Object.prototype.baseConstructor = function(_constructor, _input){
+	_constructor.call(this, _input);
+};
+
 //object constructor A
-var A = function(a){
-	this.a = a;
+var A = function(input){
+	this.a = typeof input ==  'undefined' ? null : input.a || null;
 };
 
 A.prototype = {
 	doSomething : function(){
-		console.log("there, i did[from A]!");
+		console.log("there, i did[from A]!" + this.a);
 	}
 };
 
 //object constructor B (inherits A)
-var B = function(a, b){
-	A.call(a);
-	this.b = b;
+var B = function(input){
+	this.baseConstructor(A, input);
+	this.b = typeof input ==  'undefined' ? null : input.b || null;
 };
 
 B.prototype = Object.inherit(A);
 B.prototype.doSomething = function(){
-	this.base().doSomething();
-	console.log("there, i did[from B]!");
+	console.log("there, i did[from B]!" + this.a + "-" + this.b);
 };
 
 //object constructor C (inherits B)
-var C = function(a, b, c){
-	B.call(a, b);
-	this.c = c;
+var C = function(input){
+	this.baseConstructor(B, input);
+	this.c = typeof input ==  'undefined' ? null : input.c || null;
 };
 
 C.prototype = Object.inherit(B);
 C.prototype.doSomething = function(){
-	this.base().doSomething();
-	console.log("there, i did[from C]!");
+	console.log("there, i did[from C]!" + this.a + "-" + this.b + "-" + this.c);
+};
+
+//object constructor D (inherits C)
+var D = function(input){
+	this.baseConstructor(C, input);
+	this.d = typeof input ==  'undefined' ? null : input.d || null;
+};
+
+D.prototype = Object.inherit(C);
+D.prototype.doSomethingElse = function(){
+	console.log("there, i did somthing else[from D]!" + this.a + "-" + this.b + "-" + this.c + "-" + this.d);
 };
 
 //main
 function tcm6_main(){
-	console.log("---------------");
-	z = new A(5);
-	z.doSomething();
-	console.log("---------------");
-	x = new B(5, 10);
-	x.doSomething();
-	console.log("---------------");
-	y = new C(5, 10, 15);
-	y.doSomething();
-	console.log("---------------");
+	z = new A({
+		a:1
+	});
+
+	y = new B({
+		a:2, 
+		b:1
+	});
+
+	x = new C({
+		a:3, 
+		b:2, 
+		c:1
+	});
+
+	w = new D({
+		a:4,
+		b:3,
+		c:2,
+		d:1
+	});
 }
